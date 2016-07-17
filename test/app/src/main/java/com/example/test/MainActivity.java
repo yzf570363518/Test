9412a1e93297111b3417eeb889375c5defe98644
久别再重逢 <570363518@qq.com>
2016-07-18 00:39:36 +0800

    Signed-off-by: 久别再重逢 <570363518@qq.com>

---
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.test;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+//yzf07172257
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.textview);
+        Snackbar.make(tv, "Snackbar1", Snackbar.LENGTH_LONG).setAction("Action1", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Toast1", Toast.LENGTH_SHORT).show();
+            }
+        }).show();
+    }
+}
